6d3e4ab5d6d8a6c41fba004d4062450abc096738
Aarti Jethnaiya <jethaniya20aarti@gmail.com>
2021-09-14 14:27:34 +0530

    keepnote project complete

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import CreateNote from './CreateNote';
+import Footer from './Footer';
+import Header from './Header';
+import Note from './Note';
+
+const App = () =>{
+    const [addItem, setAddItem] = useState([])
+    const addNote = (note) =>{
+        setAddItem((prevData) =>{
+            return[...prevData, note];
+        }) 
+    }
+    const onDelete = (id) =>{
+        setAddItem((olddata) =>
+            olddata.filter((currdata,indx) =>{
+                return indx !== id;
+            })
+        );
+    };
+    return (
+        <>
+            <Header />
+            <CreateNote passNote = {addNote} />
+            {addItem.map((val,index) =>{
+                return( 
+                    <Note       
+                        key = {index}
+                        id = {index}
+                        title = {val.title}
+                        content = {val.content}
+                        deleteItem = {onDelete}
+                    />
+                );
+            })}
+            <Footer />
+        </>
+    );
+}
+
+export default App;
